222c01d78149ccf837320cb140fb61caf7e6a740
Abel <github@pastur.com>
2019-08-01 19:30:06 +0100

    style landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,22 @@
 import { Head } from 'react-static'
 import React from 'react'
 
+import { Button, Flex } from '../components/styles'
+
 export default () => (
   <div>
     <Head>
       <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
     </Head>
-    
-    <h1>Abel Pastur</h1>
-    <h2>software developer</h2>
 
-    <a href="/abel-pastur-cv-2019.pdf">View CV</a>
-
-    <div className="LI-profile-badge"
-      data-version="v1"
-      data-size="large"
-      data-locale="en_US"
-      data-type="horizontal"
-      data-theme="light"
-      data-vanity="abelpastur">
+    <Flex>
+      <div className="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="vertical" data-theme="light" data-vanity="abelpastur">
         <a className="LI-simple-link" href='https://uk.linkedin.com/in/abelpastur?trk=profile-badge'>Abel Pastur</a>
-    </div>
+      </div>
+    </Flex>
+
+    <Flex style={{marginTop: '2em'}}>
+      <Button href="/abel-pastur-cv-2019.pdf">View CV</Button>
+    </Flex>
   </div>
 )
