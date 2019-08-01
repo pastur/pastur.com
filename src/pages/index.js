3fd326bb030a84c76735a86998a1cb216b140832
Abel <github@pastur.com>
2019-08-01 17:46:56 +0100

    add CV download link

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default () => (
   <div style={{ textAlign: 'center' }}>
     <h1>Abel Pastur</h1>
-    <h2>Software developer</h2>
+    <h2>software developer</h2>
+
+    <a href="/abel-pastur-cv-2019.pdf">View my CV</a>
 
     <h3>About me</h3>
     <p>I'm a full-stack web developer based in London</p>
