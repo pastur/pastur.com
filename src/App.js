125ded286b432f16385b07ad1765c06c1ce707e3
Abel <github@pastur.com>
2019-08-09 12:30:42 +0100

    refactor welcome page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react'
-import { Root, Routes, addPrefetchExcludes } from 'react-static'
-//
-import { Link, Router } from 'components/Router'
-import Dynamic from 'containers/Dynamic'
+import { Root, Routes } from 'react-static'
+
+import { Router } from 'components/Router'
 
 import './app.css'
-
-// Any routes that start with 'dynamic' will be treated as non-static routes
-addPrefetchExcludes(['dynamic'])
 
 function App() {
   return (
     <Root>
-      {/* <nav>
-        <Link to="/">Abel Pastur</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-      </nav> */}
       <div className="content">
         <React.Suspense fallback={<em>Loading...</em>}>
           <Router>
-            <Dynamic path="dynamic" />
             <Routes path="*" />
           </Router>
         </React.Suspense>
