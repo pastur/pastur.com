316847996571ab056f45c9d1a8971175d9d2aa5d
Abel <github@pastur.com>
2019-08-10 17:27:02 +0100

    refactor image loading

--- src/containers/Welcome.js
+++ src/containers/Welcome.js
@@ -1,27 +1,10 @@
 import React, { Fragment, useEffect } from "react";
 import { Head } from "react-static";
 
-export default function Welcome() {
-  function loadImage(id, targetId) {
-    var el = document.getElementById(id);
-    var targetEl = targetId ? document.getElementById(targetId) : el;
-    var imageToLoad;
-    if (el.dataset.image) {
-      imageToLoad = el.dataset.image;
-    } else if (typeof el.currentSrc === "undefined") {
-      imageToLoad = el.src;
-    } else {
-      imageToLoad = el.currentSrc;
-    }
-    if (imageToLoad) {
-      var img = new Image();
-      img.src = imageToLoad;
-      img.onload = function() {
-        targetEl.classList.add("is-loaded");
-      };
-    }
-  }
+import { loadImage } from '../utils/images'
 
+export default function Welcome() {
+  
   function loadImages() {
     loadImage("wallpaper");
     loadImage("pictureImage", "picture");
