576702975e8a14e87aab48218a16c5272b65361b
Abel Pastur <abel@grabyo.com>
2021-03-06 13:45:48 +0000

    change avatar

--- src/pages/welcome/Avatar.js
+++ src/pages/welcome/Avatar.js
@@ -1,32 +1,14 @@
-import React, { useEffect } from 'react'
-import styled from 'styled-components'
-
-import preloadImage from './utils/images'
-
-const StyledAvatar = styled.figure.attrs(() => ({ className: 'picture' }))``
-
-const AvatarImage = styled.img.attrs(() => ({
-  className: 'picture-image',
-}))``
-
-const AvatarShadow = styled.div.attrs(() => ({
-  className: 'picture-shadow',
-}))``
+import React from 'react'
 
 export default function Avatar() {
-  useEffect(
-    () => preloadImage({ id: 'picture', imgSrc: '/images/avatar.jpg' }),
-    [],
-  )
-
   return (
-    <StyledAvatar id="picture">
-      <AvatarShadow />
-      <AvatarImage
-        id="pictureImage"
+    <figure className="picture is-loaded">
+      <div className="picture-shadow" />
+      <img
+        className="picture-image"
         src="/images/avatar.jpg"
         alt="Portrait of Abel Pastur"
       />
-    </StyledAvatar>
+    </figure>
   )
 }
